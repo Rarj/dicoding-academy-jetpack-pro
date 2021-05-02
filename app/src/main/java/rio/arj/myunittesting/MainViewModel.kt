d2ed896b5b6f6b50a5fc3920c366a10ce3180fa0
Rio Arjuna <ri0arjuna@programmer.net>
2021-05-02 11:57:44 +0700

    :white_check_mark: Adding unit tests.

---
+++ app/src/main/java/rio/arj/myunittesting/MainViewModel.kt
@@ -0,0 +1,15 @@
+package rio.arj.myunittesting
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(private val cuboidModel: CuboidModel) : ViewModel() {
+  fun getCircumference() = cuboidModel.getCircumference()
+
+  fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+
+  fun getVolume() = cuboidModel.getVolume()
+
+  fun save(w: Double, l: Double, h: Double) {
+    cuboidModel.save(w, l, h)
+  }
+}
